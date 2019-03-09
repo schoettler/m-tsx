31883361dc6abf86d341ce266c71c57835ba912d
Roberto von Schoettler <roberto.vonschoettler@gmail.com>
2019-03-09 15:27:37 -0500

    initial commit

---
+++ src/components/hello.tsx
@@ -0,0 +1,11 @@
+import m from 'mithril'
+
+export const Hello = {
+  view: function () {
+    return (
+      <main>
+        <h1>hello</h1>
+      </main>
+    )
+  }
+}
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import m from 'mithril'
+import { Hello } from './components/hello'
+
+const root = document.body
+
+m.render(root, <Hello />)
